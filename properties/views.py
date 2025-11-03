6fbd31058d8edef0fde4fb5599c0e070b7a641e8
BnDaudlinux <lawalsulaimon70@gmail.com>
2025-11-03 07:01:22 +0100

    included url mapping

--- properties/views.py
+++ properties/views.py
@@ -9,4 +9,4 @@ def property_list(request):
     properties = Property.objects.all().values(
         "id", "title", "description", "price", "location", "created_at"
     )
-    return JsonResponse(list(properties), safe=False)
+    return JsonResponse({"data":list(properties)}, safe=False)
